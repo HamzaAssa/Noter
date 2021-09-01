2b6101034efe16fba305a17e1681acb0b71f986b
Hamza Assa <hamzaassarehman@gmail.com>
2021-09-01 21:54:50 +0500

    Project Done

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 "use stric";
+document.addEventListener("DOMContentLoaded", getLocalNotes);
 //selectors
-// const expand = document.querySelectorAll(".open");
 const search = document.getElementById("search");
 const create = document.getElementById("create");
 const noterHome = document.querySelector(".noter-home");
 const createArea = document.querySelector(".firstHide");
 
-//event listners
-// for (let i = 0; i < expand.length; i++) {
-//   expand[i].addEventListener("click", openNote);
-// }
-
-search.addEventListener("click", searchNote);
+search.addEventListener("click", startSearch);
 create.addEventListener("click", startNewNote);
 
+function getLocalNotes() {
+  const notes = document.querySelector(".notes");
+  notes.innerHTML = localStorage.getItem("notes");
+
+  const expand = document.querySelectorAll(".open");
+  const expandLess = document.querySelectorAll(".close");
+  const deletebutton = document.querySelectorAll(".delete");
+  const edit = document.querySelectorAll(".edit");
+
+  for (let i = 0; i < expand.length; i++) {
+    expand[i].addEventListener("click", openNote);
+  }
+  for (let i = 0; i < expandLess.length; i++) {
+    expandLess[i].addEventListener("click", closeNote);
+  }
+  for (let i = 0; i < deletebutton.length; i++) {
+    deletebutton[i].addEventListener("click", deleteNote);
+  }
+  for (let i = 0; i < edit.length; i++) {
+    edit[i].addEventListener("click", editNote);
+  }
+}
+function saveLocalNotes(note) {
+  localStorage.setItem("notes", note.parentElement.innerHTML);
+}
 //FUNCTIONS
 //startnewNote Note
 function startNewNote() {
@@ -26,12 +46,12 @@ function startNewNote() {
   createArea.appendChild(colors);
   const arr = ["blue", "green", "yellow", "red", "purple", "pink"];
   const mycolors = [
-    "#c9eaef",
-    "#7af39e",
-    "#f9e298",
-    "#f9b4b4",
-    "#a49cf1",
-    "#f599ef",
+    "rgb(201, 234, 239)",
+    "rgb(122, 243, 158)",
+    "rgb(249, 226, 152)",
+    "rgb(249, 180, 180)",
+    "rgb(164, 156, 241)",
+    "rgb(245, 153, 239)",
   ];
   for (let i = 0; i < arr.length; i++) {
     let color = document.createElement("div");
@@ -43,6 +63,10 @@ function startNewNote() {
   const title = document.createElement("textarea");
   title.classList.add("create-title");
   title.placeholder = "Title";
+  title.maxLength = 43;
+  title.rows = 1;
+  title.cols = 1;
+
   createArea.appendChild(title);
 
   const text = document.createElement("textarea");
@@ -62,6 +86,11 @@ function startNewNote() {
   saveBtn.innerText = "Save";
   bottomDiv.appendChild(saveBtn);
   createArea.appendChild(bottomDiv);
+  title.onkeyup = () => {
+    if (title.value.length >= 43) {
+      alert("Maximum length for title have reached");
+    }
+  };
   //Event Listener for bg Colors
   const bgColors = document.querySelectorAll(".color");
   for (let i = 0; i < bgColors.length; i++) {
@@ -102,24 +131,12 @@ function saveNote() {
     createArea.style.backgroundColor);
   const newDate = new Date();
   const date = newDate.getMonth() + 1 + ":" + newDate.getDate();
-  const note = new Note(title, text, date, bgColor);
+  const note = Note(title, text, date, bgColor);
   noterHome.children[1].appendChild(note);
+  saveLocalNotes(note);
   createArea.classList.remove("createArea");
   noterHome.style.display = "unset";
-
-  const expand = document.querySelectorAll(".open");
-  const expandLess = document.querySelectorAll(".close");
-  const deletebutton = document.querySelectorAll(".delete");
-
-  for (let i = 0; i < expand.length; i++) {
-    expand[i].addEventListener("click", openNote);
-  }
-  for (let i = 0; i < expandLess.length; i++) {
-    expandLess[i].addEventListener("click", closeNote);
-  }
-  for (let i = 0; i < deletebutton.length; i++) {
-    deletebutton[i].addEventListener("click", deleteNote);
-  }
+  getLocalNotes();
 }
 //Note
 function Note(title, text, date, bgColor) {
@@ -164,11 +181,11 @@ function Note(title, text, date, bgColor) {
   noteText.classList.add("text");
   noteText.innerText = text;
   note.appendChild(noteText);
-
   return note;
 }
+let c = true;
 //Search Note
-function searchNote() {
+function startSearch() {
   search.style.display = "none";
   const top = document.querySelector(".top");
   const searchInput = document.createElement("input");
@@ -177,9 +194,18 @@ function searchNote() {
   cancelSearch.src = "images/add.svg";
   cancelSearch.className = "cancel";
   top.appendChild(cancelSearch);
-  const regex = searchInput.value;
-  const h3text = document.querySelectorAll("h3").innerText;
-  const ptext = document.querySelectorAll("p").innerText;
+  searchInput.onkeyup = () => {
+    const note = document.querySelectorAll(".note");
+    const reg = searchInput.value;
+    const regex = new RegExp(reg, "gi");
+    for (let i = 0; i < note.length; i++)
+      if (note[i].innerText.match(regex)) {
+        note[i].style.display = "block";
+      } else {
+        note[i].style.display = "none";
+      }
+  };
+
   cancelSearch.onclick = () => {
     searchInput.remove();
     search.style.display = "unset";
@@ -216,5 +242,70 @@ function closeNote() {
 
 //Delete Note
 function deleteNote() {
+  this.parentElement.remove();
+  const notes = document.querySelector(".notes");
+  localStorage.setItem("notes", notes.innerHTML);
+}
+//Edit Note
+function editNote() {
+  createArea.innerHTML = "";
+  noterHome.style.display = "none";
+  createArea.classList.add("createArea");
+  createArea.style.backgroundColor = this.parentElement.style.backgroundColor;
+  const colors = document.createElement("div");
+  colors.classList.add("colors");
+  createArea.appendChild(colors);
+  const arr = ["blue", "green", "yellow", "red", "purple", "pink"];
+  const mycolors = [
+    "rgb(201, 234, 239)",
+    "rgb(122, 243, 158)",
+    "rgb(249, 226, 152)",
+    "rgb(249, 180, 180)",
+    "rgb(164, 156, 241)",
+    "rgb(245, 153, 239)",
+  ];
+  for (let i = 0; i < arr.length; i++) {
+    let color = document.createElement("div");
+    color.classList.add("color", arr[i]);
+    color.dataset.color = mycolors[i];
+    if (this.parentElement.style.backgroundColor == color.dataset.color) {
+      color.classList.add("selected");
+    }
+    colors.appendChild(color);
+  }
+  const title = document.createElement("textarea");
+  title.classList.add("create-title");
+  title.style.backgroundColor = this.parentElement.style.backgroundColor;
+  title.innerText = this.parentElement.children[5].innerText;
+  createArea.appendChild(title);
+
+  const text = document.createElement("textarea");
+  text.classList.add("create-text");
+  text.style.backgroundColor = this.parentElement.style.backgroundColor;
+  text.innerText = this.parentElement.children[6].innerText;
+  createArea.appendChild(text);
+
+  const bottomDiv = document.createElement("div");
+  bottomDiv.classList.add("bottom-dev");
+
+  const cancelBtn = document.createElement("button");
+  cancelBtn.classList.add("cancel-btn");
+  cancelBtn.innerText = "Cancel";
+  bottomDiv.appendChild(cancelBtn);
+  const saveBtn = document.createElement("button");
+  saveBtn.classList.add("save-btn");
+  saveBtn.innerText = "Save";
+  bottomDiv.appendChild(saveBtn);
+  createArea.appendChild(bottomDiv);
+  //Event Listener for bg Colors
+  const bgColors = document.querySelectorAll(".color");
+  for (let i = 0; i < bgColors.length; i++) {
+    bgColors[i].onclick = selectBg;
+  }
+
+  //event listerner for Save Notes
+  saveBtn.onclick = saveNote;
+  // Cancel Notes
+  cancelBtn.onclick = cancelNote;
   this.parentElement.remove();
 }
